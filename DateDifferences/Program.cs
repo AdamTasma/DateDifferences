13f0f0b214dc1a22ee6f386e2168c6e07dc36084
Adament13 <adamtasma@gmail.com>
2018-01-10 14:39:27 -0500

    Code works as intended, but fails if user inputs something other than the prompted format.

--- DateDifferences/Program.cs
+++ DateDifferences/Program.cs
@@ -6,7 +6,20 @@ namespace DateDifferences
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter your birthday in the format MM/DD/YYYY");
+            DateTime input1 = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine("Please enter today's date in the format MM/DD/YYYY");
+            DateTime input2 = Convert.ToDateTime(Console.ReadLine());
+            TimeSpan lifeSpan = input2.Subtract(input1);
+            //lifespan spits out how many days are between the dates
+            double inDays = lifeSpan.Days;
+            double inHours = (inDays * 24);
+            double inMins = (inHours * 60);
+            Console.WriteLine("Woah," +
+                              "\nYou have been alive for " + inDays + " Days!" +
+                              "\nThats " + inHours + " hours!" +
+                              "\nOr " + inMins + " minutes!");
+            Console.ReadLine();
         }
     }
 }
